4ba4b86b086844eb61457d0cab1fa678e68c2466
joannetsaii <chihant@andrew.cmu.edu>
2023-02-12 16:26:07 -0500

    Add power and health bars

--- Assets/Scripts/Data/WaveData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "WaveData", menuName = "ScriptableObjects/WaveData", order = 1)]
-public class WaveData : ScriptableObject
-{
-    public List<Wave> waves;
-}

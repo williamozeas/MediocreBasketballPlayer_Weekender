67c89acfde8c7e9efca856f5281e2b0b5a528d27
EfeCekirge <efecekirge@gmail.com>
2023-02-16 14:02:24 -0500

    Fixes and Right Lob

--- Assets/Scripts/Objects/Player.cs
+++ Assets/Scripts/Objects/Player.cs
@@ -23,11 +23,8 @@ public class Player : MonoBehaviour
 
     int ballDestroyTime = 5;
 
-    int shootMode;
-
     GameObject currentBall;
     Rigidbody ballRB;
-    Vector3 normalizedSideToSide;
     bool throwing = false;
     public bool IsThrowing => throwing;
     
@@ -56,7 +53,9 @@ public class Player : MonoBehaviour
     float gravity = 20f;
 
     public float rightLobAngle;
+    float rightLobAngleDeg;
     float rightLobAngleTan;
+    Vector3 finalDir;
 
     private void Awake()
     {
@@ -66,7 +65,6 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        shootMode = 1;
         chargedVelocity = minChargedVelocity;
         line.positionCount = lineNumber;
         health = maxHealth;
@@ -76,7 +74,8 @@ public class Player : MonoBehaviour
         }
         canShoot = true;
         rb = GetComponent<Rigidbody>();
-        rightLobAngleTan = MathF.Tan(rightLobAngle);
+        rightLobAngleDeg = rightLobAngle * Mathf.PI / 180;
+        rightLobAngleTan = MathF.Tan(rightLobAngleDeg);
     }
 
     private void OnEnable()
@@ -104,135 +103,30 @@ public class Player : MonoBehaviour
             shooting = true;
         }
         if (Input.GetMouseButtonUp(0) && shooting) {
-            //if (Time.time - leftTime < .15f) {
-                //mouse press not hold
-                LeftClickShoot();
-                shooting = false;
-                StartCoroutine(CoolDown());
-            //}
+            LeftClickShoot();
+            shooting = false;
+            StartCoroutine(CoolDown());
         }
 
         if (Input.GetMouseButtonDown(1) && canShoot && !shooting) {
             canShoot = false;
             shooting = true;
+            if (deleting != null) StopCoroutine(deleting);
             line.startColor = new Color(1f, .5608f, .0118f, .8f);
             line.endColor = new Color(1f, .2823f, 0f, .8f);
         }
 
         if (Input.GetMouseButton(1) && shooting)
         {
-            if(chargedVelocity < maxChargedVelocity) {
-                chargedVelocity += chargeRate * Time.deltaTime;
-            }
-            normalizedSideToSide = camTrans.forward.normalized;
-
-            Vector3 prelimVel = normalizedSideToSide * chargedVelocity;
-            prelimVel += chargedVelocity * rightLobAngleTan * Vector3.up;
-
-            DrawShootingLine(prelimVel);
+            RightHoldCharge();
         }
 
         if (Input.GetMouseButtonUp(1) && shooting) //mouse up, shoot
         {
-            deleting = EraseShootingLine(2);
-            StartCoroutine(deleting);
-            currentBall = Instantiate(ballPrefab, ballPos.position, Quaternion.identity) as GameObject;
-            ballRB = currentBall.GetComponent<Rigidbody>();
-            ballRB.useGravity = true;
-            ballRB.velocity = normalizedSideToSide * chargedVelocity;
-            ballRB.velocity += chargedVelocity * rightLobAngleTan * Vector3.up;
-            Destroy(currentBall, ballDestroyTime);
-            chargedVelocity = minChargedVelocity;
+            RightHoldShoot();
             shooting = false;
             StartCoroutine(CoolDown());
         }
-
-        /*if (Input.GetMouseButtonDown(0)) {
-            //direct shot
-            shootMode = 1;
-        }
-        if (Input.GetMouseButtonDown(1)) {
-            //arcing shot
-            shootMode = 2;
-        }
-
-        timeSinceThrow += Time.deltaTime;
-
-        if (Input.GetMouseButtonDown(0) && timeSinceThrow > cooldown || Input.GetMouseButtonDown(1) && timeSinceThrow > cooldown) {
-            if (deleting != null) StopCoroutine(deleting);
-            line.startColor = new Color(1f, .5608f, .0118f, .8f);
-            line.endColor = new Color(1f, .2823f, 0f, .8f);
-            throwing = true;
-        }
-
-        if (Input.GetMouseButton(1) && throwing && shootMode == 2) //charging
-        {
-            if(chargedVelocity < maxChargedVelocity) {
-                chargedVelocity += chargeRate * Time.deltaTime;
-            }
-            normalizedSideToSide = transform.forward.normalized;
-
-            Vector3 prelimVel = normalizedSideToSide * chargedVelocity;
-            prelimVel += chargedVelocity * 1.73f * Vector3.up;
-
-            if (handBall.transform.localPosition.x < handBallShotPos.x) handBall.transform.localPosition += handDir * handMoveSpeed;
-
-            DrawShootingLine(prelimVel);
-        } else if (Input.GetMouseButton(0) && throwing && shootMode == 1) {
-            if(chargedVelocity < maxChargedVelocity * 3) {
-                chargedVelocity += chargeRate * Time.deltaTime * 3;
-            }
-            normalizedSideToSide = transform.forward.normalized;
-
-            Vector3 prelimVel = normalizedSideToSide * chargedVelocity;
-            prelimVel += chargedVelocity * .36f * Vector3.up;
-
-            if (handBall.transform.localPosition.x < handBallShotPos.x) handBall.transform.localPosition += handDir * handMoveSpeed;
-
-            DrawShootingLine(prelimVel);
-        }
-
-        if (Input.GetMouseButtonUp(1) && throwing && shootMode == 2) //mouse up, shoot
-        {
-            handLastPos = handBall.transform.localPosition;
-            deleting = EraseShootingLine(2);
-            StartCoroutine(deleting);
-            currentBall = Instantiate(ballPrefab, transform.position, Quaternion.identity) as GameObject;
-            ballRB = currentBall.GetComponent<Rigidbody>();
-            ballRB.useGravity = true;
-            ballRB.velocity = normalizedSideToSide * chargedVelocity;
-            ballRB.velocity += chargedVelocity * 1.73f * Vector3.up;
-            Destroy(currentBall, ballDestroyTime);
-            chargedVelocity = minChargedVelocity;
-            timeSinceThrow = 0;
-            throwing = false;
-        } else if (Input.GetMouseButtonUp(0) && throwing && shootMode == 1) //mouse up, shoot
-        {
-            handLastPos = handBall.transform.localPosition;
-            deleting = EraseShootingLine(1);
-            StartCoroutine(deleting);
-            currentBall = Instantiate(ballPrefab, transform.position, Quaternion.identity) as GameObject;
-            ballRB = currentBall.GetComponent<Rigidbody>();
-            ballRB.useGravity = true;
-            ballRB.velocity = normalizedSideToSide * chargedVelocity;
-            ballRB.velocity += chargedVelocity * .36f * Vector3.up;
-            Destroy(currentBall, ballDestroyTime);
-            chargedVelocity = minChargedVelocity;
-            timeSinceThrow = 0;
-            throwing = false;
-        }
-
-        if (!throwing) {
-            float time = timeSinceThrow / cooldown;
-            float timeScale = Mathf.Clamp01(time);
-            handBall.transform.localPosition = Vector3.Slerp(handLastPos, handBallPos, timeScale);
-        }
-
-        if (shootMode == 1) {
-            percentCharge = chargedVelocity / (maxChargedVelocity * 3);
-        } else if (shootMode == 2) {
-            percentCharge = chargedVelocity / (maxChargedVelocity);
-        }*/
     }
 
     private void LeftClickShoot()
@@ -258,13 +152,40 @@ public class Player : MonoBehaviour
         Destroy(currentBall, ballDestroyTime);
     }
 
-    IEnumerator EraseShootingLine (int shootMode)
+    private void RightHoldCharge()
+    {
+        if(chargedVelocity < maxChargedVelocity) {
+            chargedVelocity += chargeRate * Time.deltaTime;
+        }
+
+        Vector3 shootDirection = camTrans.forward;
+        Vector3 shootDirectionHoriz = Vector3.Scale(shootDirection, new Vector3(1f, 0f, 1f)).normalized;
+
+        finalDir = shootDirectionHoriz * chargedVelocity;
+        finalDir += chargedVelocity * rightLobAngleTan * Vector3.up;
+
+        DrawShootingLine(finalDir);
+    }
+
+    private void RightHoldShoot()
+    {
+        deleting = EraseShootingLine();
+        StartCoroutine(deleting);
+
+        currentBall = Instantiate(ballPrefab, ballPos.position, Quaternion.identity) as GameObject;
+        ballRB = currentBall.GetComponent<Rigidbody>();
+        ballRB.useGravity = true;
+        ballRB.velocity = finalDir;
+        Destroy(currentBall, ballDestroyTime);
+        chargedVelocity = minChargedVelocity;
+    }
+
+    IEnumerator EraseShootingLine()
     {
         for (float i = .8f; i >= 0f; i -= .02f) {
             line.startColor = new Color(1f, .5608f, .0118f, i);
             line.endColor = new Color(1f, .2823f, 0f, i);
-            if (shootMode == 2) yield return new WaitForSeconds(0.05f);
-            else yield return new WaitForSeconds(0.02f);
+            yield return new WaitForSeconds(0.05f);
         }
     }
 
@@ -272,18 +193,8 @@ public class Player : MonoBehaviour
     {
         float timeToDraw = (vel.y + (float)Math.Sqrt(vel.y * vel.y + 4.0f * gravity)) / gravity;
 
-        for (int i = 0; i < 5; i++) {
-
-            Vector3 idealpos = CalculatePos(vel, timeToDraw * i / lineNumber);
-            Vector3 offset = transform.right.normalized * .5f + Vector3.down * .5f;
-            if (shootMode == 2) offset +=  Vector3.down * .5f;
-            float offsetAmt = 1f - .2f * i;
-            line.SetPosition(i, idealpos + offset * offsetAmt);
-        }
-
-        for (int i = 5; i < lineNumber; i++) {
-            line.SetPosition(i, CalculatePos(vel, timeToDraw * i / lineNumber));
-            //line.SetPosition(i, new Vector3(0, 0, i));
+        for (int i = 0; i < lineNumber; i++) {
+            line.SetPosition(i, CalculatePos(vel, timeToDraw * (float)i / (float)lineNumber));
         }
        
     }
@@ -292,7 +203,7 @@ public class Player : MonoBehaviour
     {
         Vector3 vel_h = new Vector3(vel.x, 0f, vel.z);
 
-        Vector3 pos = transform.position;
+        Vector3 pos = ballPos.position;
         pos += vel_h * t;
         pos.y += vel.y * t - (gravity / 2.0f) * t * t;
 
@@ -322,3 +233,96 @@ public class Player : MonoBehaviour
         canShoot = true;
     }
 }
+
+
+
+
+
+
+
+/*if (Input.GetMouseButtonDown(0)) {
+    //direct shot
+    shootMode = 1;
+}
+if (Input.GetMouseButtonDown(1)) {
+    //arcing shot
+    shootMode = 2;
+}
+
+timeSinceThrow += Time.deltaTime;
+
+if (Input.GetMouseButtonDown(0) && timeSinceThrow > cooldown || Input.GetMouseButtonDown(1) && timeSinceThrow > cooldown) {
+    if (deleting != null) StopCoroutine(deleting);
+    line.startColor = new Color(1f, .5608f, .0118f, .8f);
+    line.endColor = new Color(1f, .2823f, 0f, .8f);
+    throwing = true;
+}
+
+if (Input.GetMouseButton(1) && throwing && shootMode == 2) //charging
+{
+    if(chargedVelocity < maxChargedVelocity) {
+        chargedVelocity += chargeRate * Time.deltaTime;
+    }
+    normalizedSideToSide = transform.forward.normalized;
+
+    Vector3 prelimVel = normalizedSideToSide * chargedVelocity;
+    prelimVel += chargedVelocity * 1.73f * Vector3.up;
+
+    if (handBall.transform.localPosition.x < handBallShotPos.x) handBall.transform.localPosition += handDir * handMoveSpeed;
+
+    DrawShootingLine(prelimVel);
+} else if (Input.GetMouseButton(0) && throwing && shootMode == 1) {
+    if(chargedVelocity < maxChargedVelocity * 3) {
+        chargedVelocity += chargeRate * Time.deltaTime * 3;
+    }
+    normalizedSideToSide = transform.forward.normalized;
+
+    Vector3 prelimVel = normalizedSideToSide * chargedVelocity;
+    prelimVel += chargedVelocity * .36f * Vector3.up;
+
+    if (handBall.transform.localPosition.x < handBallShotPos.x) handBall.transform.localPosition += handDir * handMoveSpeed;
+
+    DrawShootingLine(prelimVel);
+}
+
+if (Input.GetMouseButtonUp(1) && throwing && shootMode == 2) //mouse up, shoot
+{
+    handLastPos = handBall.transform.localPosition;
+    deleting = EraseShootingLine(2);
+    StartCoroutine(deleting);
+    currentBall = Instantiate(ballPrefab, transform.position, Quaternion.identity) as GameObject;
+    ballRB = currentBall.GetComponent<Rigidbody>();
+    ballRB.useGravity = true;
+    ballRB.velocity = normalizedSideToSide * chargedVelocity;
+    ballRB.velocity += chargedVelocity * 1.73f * Vector3.up;
+    Destroy(currentBall, ballDestroyTime);
+    chargedVelocity = minChargedVelocity;
+    timeSinceThrow = 0;
+    throwing = false;
+} else if (Input.GetMouseButtonUp(0) && throwing && shootMode == 1) //mouse up, shoot
+{
+    handLastPos = handBall.transform.localPosition;
+    deleting = EraseShootingLine(1);
+    StartCoroutine(deleting);
+    currentBall = Instantiate(ballPrefab, transform.position, Quaternion.identity) as GameObject;
+    ballRB = currentBall.GetComponent<Rigidbody>();
+    ballRB.useGravity = true;
+    ballRB.velocity = normalizedSideToSide * chargedVelocity;
+    ballRB.velocity += chargedVelocity * .36f * Vector3.up;
+    Destroy(currentBall, ballDestroyTime);
+    chargedVelocity = minChargedVelocity;
+    timeSinceThrow = 0;
+    throwing = false;
+}
+
+if (!throwing) {
+    float time = timeSinceThrow / cooldown;
+    float timeScale = Mathf.Clamp01(time);
+    handBall.transform.localPosition = Vector3.Slerp(handLastPos, handBallPos, timeScale);
+}
+
+if (shootMode == 1) {
+    percentCharge = chargedVelocity / (maxChargedVelocity * 3);
+} else if (shootMode == 2) {
+    percentCharge = chargedVelocity / (maxChargedVelocity);
+}*/
--- Assets/Scripts/Player/Rotate.cs
+++ Assets/Scripts/Player/Rotate.cs
@@ -47,7 +47,7 @@ public class Rotate : MonoBehaviour
 
             yRotation += mouseX;
             xRotation -= mouseY;
-            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            xRotation = Mathf.Clamp(xRotation, -70f, 70f);
 
             transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
             orientation.rotation = Quaternion.Euler(0, yRotation, 0);
